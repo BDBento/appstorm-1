761e6184ff3f84733840846d6d06fb6ce0482900
BDBento <66705552+BDBento@users.noreply.github.com>
2021-05-06 16:17:21 -0400

    bora

--- functions.php
+++ functions.php
@@ -4,4 +4,4 @@ function load_scripts(){
     wp_enqueue_scritp('script-personalizado',get_template_directory_uri().'/assets/css/style-personalizado.css');
 }
 
-add_action( 'wp_enqueue_scritp','load_scripts'),
+add_action( 'wp_enqueue_scritp','load_scripts');
--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php get_header()?>
 
 
+<!--para chamar a versao(wp) e nome da pg -->
+<?php 
+    bloginfo( 'name' ); 
+    echo '</br>';
+    bloginfo( 'version' );
+    echo '</br>';
+?>
+<!--para chamar pousts caso tenha -->
+
+<?php if(have_posts(  )):
+            while(have_posts(  )):the_post(  );
+                    echo 'nao tem post';
+            endwhile;
+        endif;
+    
+?>
+
 
 
 <?php get_footer()?>
